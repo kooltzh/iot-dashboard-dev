262d2288342a96ce455bb6e9f5b88ee7b3f1f522
kooltzh <kooltzh@gmail.com>
2019-01-02 10:58:24 +0800

    added dbconfig template

---
+++ dbconfig.py
@@ -0,0 +1,8 @@
+# Config for initializing influxdb connection using InfluxDBClient
+
+influx_host = ''
+influx_port = ''
+influx_db = ''
+
+# Default host is localhost, port is 8086
+# Change the influx_db to the name of database that used in influxdb
