9dfd0fa6004b2ad7c6489ff389a7bdba27bd029c
WuCong Lim <wucong@live.com>
2019-01-09 18:25:51 +0800

    configuration of database

--- dbconfig.py
+++ dbconfig.py
@@ -1,8 +1,8 @@
 # Config for initializing influxdb connection using InfluxDBClient
 
-influx_host = ''
-influx_port = ''
-influx_db = ''
+influx_host = 'localhost'
+influx_port = '8086'
+influx_db = 'pyexample'
 
 # Default host is localhost, port is 8086
 # Change the influx_db to the name of database that used in influxdb
