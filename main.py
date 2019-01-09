75fcfbf3ac50b69993ab85603fd41904fc36e162
WuCong Lim <wucong@live.com>
2019-01-09 18:26:05 +0800

    server file

--- main.py
+++ main.py
@@ -5,14 +5,40 @@ from flask import render_template
 from flask import request
 from flask import Response
 
+
 from influxdb import InfluxDBClient
 import dbconfig
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State, Event
+import plotly.plotly as py
+from plotly.graph_objs import *
+from scipy.stats import rayleigh
+from flask import Flask
+import numpy as np
+import pandas as pd
+import os
+import datetime as dt
+
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+                "https://fonts.googleapis.com/css?family=Raleway:400,400i,700,700i",
+                "https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i"]
+
+
 import time
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+app = dash.Dash(
+    'streaming-wind-app',
+    external_stylesheets=external_css
+)
 
 client = InfluxDBClient(host=dbconfig.influx_host, port=dbconfig.influx_port)
+
+
 
 #check if database already exist, if not create it
 list_databases = client.get_list_database()
@@ -32,7 +58,8 @@ data_dict = {
         "location": "SPIRIT_lab"
     },
     "fields": {
-        "temperature": ""
+        "temperature": "",
+        "humidity": ""
     }
 }
 
@@ -42,14 +69,65 @@ def web_print():
 
 @app.route("/api/post", methods=['GET', 'POST'])
 def get_post():
-    val = request.form.get('maindata')
-    print ("POST received {}".format(val))
-    if val is not None:
-        data_dict['fields']['temperature'] = float(val)
+    val_tem = request.form.get('temperature')
+    val_hmd = request.form.get('humidity')
+    print ("POST received temperature {}".format(val_tem))
+    print ("POST received humidity {}".format(val_hmd))
+
+    if val_tem is not None:
+        data_dict['fields']['temperature'] = float(val_tem)
+       # data_dict['fields']['humidity'] = float(val_hmd)
         client.write_points([data_dict])
         return_text = 'success'
         resp = Response(return_text, status=200, mimetype='text/plain')
         return resp
+
+#    elif val_hmd is not None:
+#        data_dict['fields']['humidity'] = float(val_hmd)
+#        client.write_points([data_dict])
+#        return_text = 'success'
+#        resp = Response(return_text, status=200, mimetype='text/plain')
+#        return resp
+
+    else:
+        return_text = 'fail'
+        resp = Response(return_text, status=-10, mimetype='text/plain')
+        return resp
+
+#def get_post_hmd():
+    #val_hmd = request.form.get('humidity')
+    #print ("POST received humidity {}".format(val_hmd))
+#    elif val_hmd is not None:
+#        data_dict['fields']['humidity'] = float(val_hmd)
+#        client.write_points([data_dict])
+#        return_text = 'success'
+#        resp = Response(return_text, status=200, mimetype='text/plain')
+#        return resp
+#    else:
+#        return_text = 'fail'
+#        resp = Response(return_text, status=-10, mimetype='text/plain')
+#        return resp
+
+
+@app.route("/api/hmd", methods=['GET', 'POST'])
+def get_hmd():
+    val_hmd = request.form.get('humidity')
+    print ("POST received humidity {}".format(val_hmd))
+
+    if val_hmd is not None:
+        data_dict['fields']['humidity'] = float(val_hmd)
+        client.write_points([data_dict])
+        return_text = 'success'
+        resp = Response(return_text, status=200, mimetype='text/plain')
+        return resp
+
+#    elif val_hmd is not None:
+#        data_dict['fields']['humidity'] = float(val_hmd)
+#        client.write_points([data_dict])
+#        return_text = 'success'
+#        resp = Response(return_text, status=200, mimetype='text/plain')
+#        return resp
+
     else:
         return_text = 'fail'
         resp = Response(return_text, status=-10, mimetype='text/plain')
